f0a7d10425be1194293ac9df419377e1fa893ef3
NormanAcadamay <korawit4zx@gmail.com>
2024-03-04 22:18:28 +0700

    fontback

---
+++ src/layout/Header.jsx
@@ -0,0 +1,49 @@
+import {Link, useNavigate} from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
+
+const guestNav = [
+  { to : '/', text: 'หน้าแรก' },
+  { to : '/register', text: 'เติมเงิน' },
+  { to : '/', text: 'Login' },
+  { to : '/register', text: 'Sing-up' },
+
+]
+
+const userNav = [
+  { to : '/', text: 'Home' },
+  { to : '/new', text: 'New Todo' },
+]
+
+export default function Header() {
+  const {user, logout} = useAuth()
+  const finalNav = user?.id ? userNav : guestNav
+
+  const navigate = useNavigate()
+
+  const hdlLogout = () => {
+    logout()
+    navigate('/')
+  }
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Sever {user?.id ? user.username : '.'}</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          { finalNav.map( el => (
+            <li key={el.to} >
+              <Link to={el.to}>{el.text}</Link>
+            </li>
+          ))}
+          { user?.id && (
+            <li>
+              <Link to='#' onClick={hdlLogout}>Logout</Link>
+            </li>
+          ) }
+        </ul>
+      </div>
+    </div>
+  );
+}
